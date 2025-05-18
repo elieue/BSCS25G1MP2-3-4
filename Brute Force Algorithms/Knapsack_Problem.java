ac5009862eb08740849788ab749b2667d0477828
RavenJess27 <132120812+RavenJess27@users.noreply.github.com>
2025-05-18 20:30:11 +0800

    Updated cohesion + added comments (Might be a prototype of the code?)

--- Brute Force Algorithms/Knapsack_Problem.java
+++ Brute Force Algorithms/Knapsack_Problem.java
@@ -4,17 +4,17 @@ public class Knapsack_Problem {
     static Scanner objScanner = new Scanner(System.in);
 
     public static int funcKnapSackRecursive(int intCapacity, int[] intArrVal, int[] intArrWeight, int intArraySize) {
-        if (intArraySize == 0 || intCapacity == 0) return 0;
+        if (intArraySize == 0 || intCapacity == 0) return 0; // If there's nothing else to grab or if the knapsack is full, return
 
-        int intChosen = 0;
+        int intChosen = 0; // Chosen Item
 
-        // Pick nth item if it does not exceed the capacity of knapsack
-        if (intArrWeight[intArraySize - 1] <= intCapacity)
-            intChosen = intArrVal[intArraySize - 1] + funcKnapSackRecursive(intCapacity - intArrWeight[intArraySize - 1], intArrVal, intArrWeight, intArraySize - 1);
+        // Picks the n'th item if it does not exceed the capacity of knapsack, recursively checks again with reduced space and selection
+        if (intArrWeight[intArraySize - 1] <= intCapacity) intChosen = intArrVal[intArraySize - 1] + funcKnapSackRecursive(intCapacity - intArrWeight[intArraySize - 1], intArrVal, intArrWeight, intArraySize - 1);
         
-        // Don't pick the nth item
+        // Checks and goes over to the path that is not chosen if there are better options
         int intNotChosen = funcKnapSackRecursive(intCapacity, intArrVal, intArrWeight, intArraySize - 1);
-         
+        
+        // Picks which between the chosen and not chosen in the current call is greater 
         return Math.max(intChosen, intNotChosen);
     }
 
@@ -23,17 +23,17 @@ public class Knapsack_Problem {
         return funcKnapSackRecursive(intCapacity, intArrVal, intArrWeight, intArraySize);
     }
 
-    public static int funcIntOption(String prompt) {
-        boolean boolValidOption = false;
-        int intOption = 0;
-        System.out.print(prompt);
-        do {
-            String input = objScanner.nextLine();
+    public static int funcIntOption(String strPrompt) {
+        boolean boolValidOption = false; // Initialize the loop
+        int intOption = 0; // Initialize current value
+        System.out.print(strPrompt);
+        do { // Do a loop to check if the user inputted an actual integer, return true if done correctly, reloops if invalid (character/float/etc)
+            String strInput = objScanner.nextLine();
             try {
-                intOption = Integer.parseInt(input);
-                if (intOption <= 0) System.out.print("Option must be positive. Please enter an integer above 0: ");
+                intOption = Integer.parseInt(strInput); // Parse the input into an integer
+                if (intOption <= 0) System.out.print("Option must be positive. Please enter an integer above 0: "); // Limitted every value to not be zero, we don't want zero value and weighted elements, zero capacity knapsack and zero selection elements now, do we? 
                 else boolValidOption = true;
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException e) { // If what is inputted is NOT an integer, catch the exception, preventing the program to close and instead, show an error message
                 System.out.print("Invalid input. Please enter an integer: ");
             }
         } while (!boolValidOption);
@@ -41,29 +41,29 @@ public class Knapsack_Problem {
         return intOption;
     }
 
-    public static int funcIntOptionRange(String prompt, int min, int max) {
+    public static int funcIntOptionRange(String strPrompt, int intMin, int intMax) { // Ditto from funcIntOption except with domain and range since I'm lazy :P
         boolean boolValidOption = false;
         int intOption = 0;
-        System.out.print(prompt);
+        System.out.print(strPrompt);
         do {
-            String input = objScanner.nextLine();
+            String strInput = objScanner.nextLine();
             try {
-                intOption = Integer.parseInt(input);
-                if (intOption < min || intOption > max) System.out.printf("Option is out of bounds. Please enter an integer from %d-%d: ", min, max);
+                intOption = Integer.parseInt(strInput);
+                if (intOption < intMin || intOption > intMax) System.out.printf("Option is out of bounds. Please enter an integer from %d-%d: ", intMin, intMax);
                 else boolValidOption = true;
             } catch (NumberFormatException e) {
-                System.out.printf("Invalid input. Please enter an integer from %d-%d: ", min, max);
+                System.out.printf("Invalid input. Please enter an integer from %d-%d: ", intMin, intMax);
             }
         } while (!boolValidOption);
 
         return intOption;
     }
 
-    public static void funcShowKnapSack(int[] intArrVal, int[] intArrWeight, int intArraySize) {
+    public static void funcShowKnapSack(int[] intArrVal, int[] intArrWeight, int intArraySize) { // Shows the selection elements, made it into a method for convenience
         System.out.println("Value:");
-        for (int j = 0; j < intArraySize; j++) System.out.print(intArrVal[j] + "\t");
+        for (int i = 0; i < intArraySize; i++) System.out.print(intArrVal[i] + "\t");
         System.out.println("\nWeight:");
-        for (int k = 0; k < intArraySize; k++) System.out.print(intArrWeight[k] + "\t");
+        for (int j = 0; j < intArraySize; j++) System.out.print(intArrWeight[j] + "\t");
         System.out.print("\n");
     }
 
